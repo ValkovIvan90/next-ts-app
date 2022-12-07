37cd2a6dba72ac73188542b5098e38f3094530bc
Johnny738 <ivan.valkov@securikett.com>
2022-12-07 20:48:41 +0100

    blog added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Album from "../components/Albume";
+import Blog from "../components/Blog/Blog";
 
 export default function Home() {
   return (
     <div>
+      <Blog/>
       <Album/>
     </div>
   );
