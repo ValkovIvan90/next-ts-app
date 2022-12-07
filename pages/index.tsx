e0ca9681f599f8a5ed86afd5dd3eda1b5c36930f
Johnny738 <ivan.valkov@securikett.com>
2022-12-07 22:13:27 +0100

    d..

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
-import Album from "../components/Albume";
 import Blog from "../components/Blog/Blog";
 
 export default function Home() {
   return (
     <div>
       <Blog/>
-      <Album/>
     </div>
   );
 }
