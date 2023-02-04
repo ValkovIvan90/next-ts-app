2c6c8f762f0cb5c831788a9b29e7a82831a7a371
Ivan Valkov <johnsson738@gmail.com>
2023-02-05 00:18:01 +0100

    add firebase data

---
+++ data/local_data.js
@@ -0,0 +1,15 @@
+
+
+const localData = {
+  mainFeaturedPost: {
+    title: 'Title of a longer featured blog post',
+    description:
+      "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvJavn8_cVqnqpuUvOXo3rFO5ia7J9hehV6w&usqp=CAU',
+    imageText: 'main image description',
+    linkText: 'Continue reading…',
+  },
+}
+
+
+export default localData;
